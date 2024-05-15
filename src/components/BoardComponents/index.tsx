ddba9be69746734eb508d14f9486735f85a06f2b
DavidParrado <juandparrado04@gmail.com>
2024-05-15 17:45:53 -0500

    Nodes component added

--- src/components/BoardComponents/index.tsx
+++ src/components/BoardComponents/index.tsx
@@ -1,6 +1,15 @@
-import { Component, createSignal, onMount } from "solid-js";
+import { Accessor, Component, For, Setter, createSignal, onMount } from "solid-js";
 import styles from './styles.module.css'
 import ButtonsComponent from "../ButtonsComponent";
+import { NodeComponent } from "../NodeComponent";
+
+interface Node {
+  id: string;
+  numberInputs: number;
+  numberOutputs: number;
+  prevPosition: { get: Accessor<{ x: number, y: number }>; set: Setter<{ x: number, y: number }> }
+  currPosition: { get: Accessor<{ x: number, y: number }>; set: Setter<{ x: number, y: number }> }
+}
 
 const BoardComponent: Component = () => {
 
@@ -8,6 +17,8 @@ const BoardComponent: Component = () => {
   const [scale, setScale] = createSignal<number>(1);
   const [clickedPosition, setClickedPosition] = createSignal({ x: -1, y: -1 });
   const [selectedNode, setSelectedNode] = createSignal<string | null>(null);
+
+  const [nodes, setNodes] = createSignal<Node[]>([]);
 
 
   onMount(() => {
@@ -33,6 +44,9 @@ const BoardComponent: Component = () => {
   })
 
   const handleOnMouseDownBoard = (event: any) => {
+    // Deselect node
+    setSelectedNode(null);
+
     // Start grabbing board
     setGrabbingBoard(true);
     setClickedPosition({ x: event.x, y: event.y })
@@ -51,20 +65,88 @@ const BoardComponent: Component = () => {
       const deltaX = event.x - clickedPosition().x;
       const deltaY = event.y - clickedPosition().y;
 
-      const boardWrapperElement = document.getElementById('boardWrapper');
-      if (boardWrapperElement) {
-        boardWrapperElement.scrollBy(-deltaX, -deltaY);
-        setClickedPosition({ x: event.x, y: event.y });
+      if (selectedNode() !== null) {
+        const deltaX = event.x - clickedPosition().x;
+        const deltaY = event.y - clickedPosition().y;
+
+        const node = nodes().find(node => node.id === selectedNode());
+        if (node) {
+          // Update node position
+          node.currPosition.set((_) => ({
+            x: (node.prevPosition.get().x + deltaX) / scale(),
+            y: (node.prevPosition.get().y + deltaY) / scale(),
+          }))
+        }
+
       }
+      else {
+        const boardWrapperElement = document.getElementById('boardWrapper');
+        if (boardWrapperElement) {
+          boardWrapperElement.scrollBy(-deltaX, -deltaY);
+          setClickedPosition({ x: event.x, y: event.y });
+        }
+      }
+
+
 
     }
   }
 
-  function handleOnClickAdd(numberInputs: number, numberOutputs: number) {
-  
+  const handleOnClickAdd = (numberInputs: number, numberOutputs: number) => {
+    const randomX = Math.random() * window.innerWidth;
+    const randomY = Math.random() * window.innerHeight;
+
+    const [nodePrev, setNodePrev] = createSignal<{ x: number; y: number }>({ x: randomX, y: randomY });
+    const [nodeCurr, setNodeCurr] = createSignal<{ x: number; y: number }>({ x: randomX, y: randomY });
+
+    setNodes([
+      ...nodes(),
+      {
+        id: `node_${Math.random().toString(36).substring(2, 8)}`,
+        numberInputs: numberInputs,
+        numberOutputs: numberOutputs,
+        prevPosition: { get: nodePrev, set: setNodePrev },
+        currPosition: { get: nodeCurr, set: setNodeCurr },
+      }]);
+
   }
 
-  function handleOnClickDelete() {}
+  const handleOnClickDelete = () => {
+    const node = nodes().find(node => node.id === selectedNode())
+    if (!node) return;
+    setNodes([...nodes().filter((node => node.id !== selectedNode()))]);
+    setSelectedNode(null);
+  }
+
+  const handleOnMouseDownNode = (id: string, event: any) => {
+    // Select node
+    setSelectedNode(id);
+
+    // Update first click position
+    setClickedPosition({ x: event.x, y: event.y });
+
+    const node = nodes().find(node => node.id === selectedNode())
+    if (node) {
+      // Update node position
+      node.prevPosition.set((_) => ({
+        x: node.currPosition.get().x * scale(),
+        y: node.currPosition.get().y * scale()
+      }))
+    }
+
+  }
+
+  const handleOnMouseDownOutput = (outputPositionX: number, outputPositionY: number, nodeId: string, outputIndex: number) => {
+
+  }
+
+  const handleOnMouseEnterInput = (inputPositionX: number, inputPositionY: number, nodeId: string, inputIndex: number) => {
+
+  }
+
+  const handleOnMouseLeaveInput = (nodeId: string, inputIndex: number) => {
+
+  }
 
 
 
@@ -76,6 +158,22 @@ const BoardComponent: Component = () => {
         onMouseDown={handleOnMouseDownBoard}
         onMouseUp={handleOnMouseUpBoard}
         onMouseMove={handleOnMouseMove}>
+        <For each={nodes()}>
+          {(node: Node) => (
+            <NodeComponent
+              id={node.id}
+              x={node.currPosition.get().x}
+              y={node.currPosition.get().y}
+              numberInputs={node.numberInputs}
+              numberOutputs={node.numberOutputs}
+              selected={selectedNode() === node.id}
+              onMouseDownNode={handleOnMouseDownNode}
+              onMouseDownOutput={handleOnMouseDownOutput}
+              onMouseEnterInput={handleOnMouseEnterInput}
+              onMouseLeaveInput={handleOnMouseLeaveInput}
+            />
+          )}
+        </For>
       </div>
     </div>
   )
